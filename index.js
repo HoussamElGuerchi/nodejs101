99b0819fa9ba0dab011fff9b8a7ba6cd3b3b3fb3
Houssam EL GUERCHI <houssamelgu@gmail.com>
2021-10-17 01:09:38 +0100

    complete basics

---
+++ index.js
@@ -0,0 +1,22 @@
+const http = require("http");
+const fs = require("fs");
+const url = require("url");
+
+http.createServer((req, res) => {
+    console.log("Listening on port 8800");
+
+    const query = url.parse(req.url, true);
+    const filename = (query.pathname === "/") ? "./index.html" : `./pages${query.path}.html`;
+
+    fs.readFile(filename, (err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.writeHead(200,
+                { 'Content-Type': 'text/html' }
+            );
+            res.write(data);
+            res.end();
+        }
+    })
+}).listen(8800);
